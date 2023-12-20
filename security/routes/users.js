831490b39a192e5f9bc527d37abfed63903e00fd
Cliente CONTROL-PC <stesherr@espol.edu.ec>
2023-12-20 10:44:00 -0500

    Guia 20 incomplete

--- security/routes/users.js
+++ security/routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* Módulo jsonwebtoken */
+const jwt = require('jsonwebtoken');
+
 /* Módulo crypto */
 let crypto = require('crypto');
 
@@ -45,6 +48,34 @@ router.post('/register', async (req, res,next) => {
   }
 
 })
+
+router.post('/generateToken', async (req, res,next) => {
+
+  // Parámetros en el cuerpo del requerimiento
+  let { name, password } = req.body;
+
+  try {
+
+  // Encripte la contraseña
+  let salt = process.env.SALT
+  let hash = crypto.createHmac('sha512', salt).update(password).digest("base64");
+  let passwordHash = salt + "$" + hash
+
+  /* Obtenga el usuario y su rol */
+  let user = await Users.findOne({ where: { [Op.and]: [ { name: name }, { password: passwordHash } ] } })
+  let relations = await UsersRoles.findOne({ where: { [Op.and]: [ { users_iduser: user.iduser } ] } });
+  let roles = await Roles.findOne({ where: { [Op.and]: [ { idrole: relations.roles_idrole } ] } });
+
+  /* Genera el token con los datos encriptados */
+  const accessToken = jwt.sign({ name: user.name, role: roles.name }, process.env.TOKEN_SECRET);
+
+  res.json({ accessToken });
+
+  } catch (error) {
+      res.status(400).send(error)
+  }
+
+});
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
